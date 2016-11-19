cf05777e331a78540dd230ae2905b3e91217fa19
nfrush <nicholas.frush@gmail.com>
2016-11-19 17:13:53 -0500

    Initial Commit

---
+++ database/rethink.go
@@ -0,0 +1,32 @@
+package rethink
+
+import (
+	"fmt"
+	"log"
+
+	r "github.com/dancannon/gorethink"
+)
+
+var (
+	session *r.Session
+)
+
+func init() {
+	var err error
+
+	session, err = r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "test",
+		MaxOpen:  40,
+	})
+	if err != nil {
+		log.Fatalln(err.Error())
+	} else {
+		fmt.Println("Successfully connected to the RethinkDB")
+	}
+
+  //GetSession - Gets Current RethinkDB Session
+  func GetSession() *r.Session {
+	  return session
+  }
+}
